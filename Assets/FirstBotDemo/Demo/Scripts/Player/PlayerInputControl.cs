ef25c878ea9a9fef4364898bd372deb3f1447ffe
vontell <vontell@mit.edu>
2023-11-15 15:02:58 -0500

    Initial commit with RG code included

---
+++ Assets/FirstBotDemo/Demo/Scripts/Player/PlayerInputControl.cs
@@ -0,0 +1,38 @@
+using RegressionGames;
+using UnityEngine;
+
+namespace FirstBotDemo
+{
+    public class PlayerInputControl : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+
+        [Header("Movement Settings")]
+        public bool analogMovement;
+
+        [Header("Bot Settings")] public bool isBot = true;
+
+        private void Update()
+        {
+            if (!isBot)
+            {
+                MoveInput(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+                JumpInput(Input.GetButtonDown("Jump"));
+            }
+        }
+
+        [RGAction("MoveInDirection")]
+        public void MoveInput(Vector2 newMoveDirection)
+        {
+            move = newMoveDirection;
+        }
+        
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
+    }
+}
